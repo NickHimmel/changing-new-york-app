c797ecc44499c142d5f71cd48a3d261f861f9a5d
NickHimmel <himmel.nick@gmail.com>
2018-12-18 12:13:10 -0500

    Add Map component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from './components/Map.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <Map />
       </div>
     );
   }
