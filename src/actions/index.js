844988b4cbba65e1235e971383e70f2ac70266dd
NickHimmel <himmel.nick@gmail.com>
2018-12-19 22:35:16 -0500

    Add fetchPhoto action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 import axios from 'axios';
+
+export const fetchPhoto = (uuid) => {
+  return (dispatch, getState) => {
+
+    axios.get(`https://changing-new-york-api.herokuapp.com/photos/{uuid}`)
+      .then(res => {
+        dispatch(fetchPhotoSuccess(res.data))
+      })
+      .catch(error => {
+        throw(error)
+      });
+  };
+};
