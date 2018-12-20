18563fb5018736b6b3c7632a42309cb2e2b3326f
NickHimmel <himmel.nick@gmail.com>
2018-12-20 16:56:10 -0500

    Add photosReducer boilerplate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,15 @@
-export const photosReducer = (state = '', action) => {
+export const photosReducer = (state = {isFetching: false, photos: []}, action) => {
   switch (action.type) {
-    case FETCH_PHOTOS
-    default
+    case 'START_PHOTOS_FETCH':
+      return {
+        isFetching: true,
+      };
+    case 'FETCH_PHOTOS':
+      return {
+        isFetching: false,
+        photos: action.photo
+      };
+    default:
+      return state;
   }
 }
