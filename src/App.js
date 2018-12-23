45d42710dd82d3e5acb4e0baaf6bff6210db33ba
NickHimmel <himmel.nick@gmail.com>
2018-12-22 22:02:40 -0500

    remove constructor from app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import Comparison from './components/Comparison.js'
 require('dotenv').config();
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    
-  }
-
   render() {
     return (
       <div>
