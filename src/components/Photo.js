2731a5c1e89fd0f794399076ee73a574e243221a
NickHimmel <himmel.nick@gmail.com>
2018-12-23 22:29:10 -0500

    Set up photo so it displays information from api

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Photo = (props) => {
-  console.log(props.data.uuid)
+
   return (
     <div>
-      <h1>Title</h1>
-      <h2>Note</h2>
-      <h3>photographer</h3>
-      <p>date</p>
+      <h1>{props.data.title}</h1>
+      <h2>{props.data.note}</h2>
+      <a href={props.data.item_link} target="_blank"><img src={props.data.image} width="500px"/></a>
+      <h3>{props.data.photographer}, {props.data.date}</h3>
+      <a href={props.data.rights_link} target="_blank">{props.data.rights}</a>
     </div>
   )
 }
