76050091afc37e60b6c6c85d857b521e51dc0174
NickHimmel <himmel.nick@gmail.com>
2018-12-24 13:52:45 -0500

    Clean up delinter errors

--- src/components/Footnotes.js
+++ src/components/Footnotes.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const Footnotes = (props) => {
-  console.log(props.data);
   return (
     <div>
       <h3>Footnotes</h3>
       {props.data.map((footnote, i) => {
-        return <a href={footnote.link} target="_blank" key={i}>{i + 1}</a>
+        return <a href={footnote.link} target="_blank" key={i} rel="noopener noreferrer">{i + 1}</a>
       })}
     </div>
   )
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,9 +6,9 @@ const Photo = (props) => {
     <div>
       <h1>{props.data.title}</h1>
       <h2>{props.data.note}</h2>
-      <a href={props.data.item_link} target="_blank"><img src={props.data.image} width="500px"/></a>
+      <a href={props.data.item_link} target="_blank" rel="noopener noreferrer"><img src={props.data.image} alt={props.data.title} width="500px"/></a>
       <h3>{props.data.photographer}, {props.data.date}</h3>
-      <a href={props.data.rights_link} target="_blank">{props.data.rights}</a>
+      <a href={props.data.rights_link} target="_blank" rel="noopener noreferrer">{props.data.rights}</a>
     </div>
   )
 }
