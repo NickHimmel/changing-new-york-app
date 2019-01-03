bd1d913b651ef9ff0c3f4fa2a7125b382cf3ab53
NickHimmel <himmel.nick@gmail.com>
2019-01-03 12:43:10 -0500

    Remove react router from code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route, Switch, HashRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
 import './index.css';
 import App from './App';
-import Photos from './components/Photos.js'
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore()
 
 render(
   <Provider store={store}>
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={App}/>
-        <Route exact path="/photos/:id" component={Photos}/>
-      </Switch>
-    </HashRouter>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
