e0674c35c1715177b9c495a42423ade3ebce4815
NickHimmel <himmel.nick@gmail.com>
2019-01-02 21:32:48 -0500

    Add css to photo

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -5,10 +5,10 @@ const Photo = (props) => {
 
   return (
     <div className="photo">
-      <h1 className={styles.title}>{props.data.title}</h1>
-      <h2>{props.data.note}</h2>
-      <a href={props.data.item_link} target="_blank" rel="noopener noreferrer"><img className="photo-image" src={props.data.image} alt={props.data.title}/></a>
-      <h3>{props.data.photographer}, {props.data.date}</h3>
+      <h1>{props.data.title}</h1>
+      <a href={props.data.item_link} target="_blank" rel="noopener noreferrer"><img className={styles.image} src={props.data.image} alt={props.data.title}/></a>
+      <h2 className={styles.note}>{props.data.note}</h2>
+      <h3 className={styles.photographer}>{props.data.photographer}, {props.data.date}</h3>
       <a href={props.data.rights_link} target="_blank" rel="noopener noreferrer">{props.data.rights}</a>
     </div>
   )
