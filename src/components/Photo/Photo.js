f41c28369151d249b7506f190405b67aeaa80442
NickHimmel <himmel.nick@gmail.com>
2019-01-07 10:47:10 -0500

    Update styling on captions

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -14,7 +14,7 @@ const Photo = (props) => {
       <a className={styles.link} href={props.data.item_link} target="_blank" rel="noopener noreferrer">
         <img className={styles.image} src={props.data.image} alt={props.data.title}/>
       </a>
-      <h1 className={styles.note}>{props.data.title} {props.data.note}</h1>
+      <h1 className={styles.title}>{props.data.title} <span className={styles.note}>{props.data.note}</span></h1>
       <h3 className={styles.photographer}>{props.data.photographer}, <span className={styles.date}>{longDate(props.data.date)}</span></h3>
       <a className={styles.rightsLink} href={props.data.rights_link} target="_blank" rel="noopener noreferrer">{props.data.rights}</a>
     </div>
