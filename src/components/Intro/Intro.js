b5b7fd1ebfb70af6ec201384c71c9544f764f797
NickHimmel <himmel.nick@gmail.com>
2019-01-08 16:34:32 -0500

    Add grid to intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Intro.module.css';
 
 const Intro = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Changing New York</h1>
       <p></p>
     </div>
