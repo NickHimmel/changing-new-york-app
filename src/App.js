7476913962306ba5aa4b8eac30ec5739e713199f
NickHimmel <himmel.nick@gmail.com>
2019-01-08 14:25:53 -0500

    Add intro component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'normalize.css';
+import Intro from './components/Intro/Intro.js'
 import Map from './components/Map/Map.js';
 import Photos from './components/Photos/Photos.js';
 import styles from './App.module.css';
 
 require('dotenv').config();
 
-const App = () => {  
+const App = () => {
   return (
     <div className={styles.container}>
+      <Intro />
       <Map />
       <Photos />
     </div>
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Changing New York</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default Intro;
