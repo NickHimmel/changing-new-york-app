4689acea315b8b04b757027d670c504cdac560a3
NickHimmel <himmel.nick@gmail.com>
2019-01-13 21:56:42 -0500

    Refact map and add markers with hover and click effects

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -9,44 +9,20 @@ class Map extends Component {
   constructor(props) {
     super (props);
 
-    this.state = {
-      features: [],
-      feature: {},
-      id: 10
-    }
+    this.state = {id: 10}
   }
 
   handleClick = (uuid) => {
     this.props.fetchPhotos(uuid);
   }
 
-  getFeatures = (e, map) => {
-    const features = map.queryRenderedFeatures(e.point, {
-       layers: ['changing-new-york']
-     });
-
-    this.setState({
-      features: features,
-      feature: features[0]
-    });
-  }
-
-  activeFeature = (map, id) => {
-    this.setState({
-      id: id
-    })
-
-    map.setPaintProperty('changing-new-york', 'circle-color', ['case', ['==', ['id'], this.state.id], '#db4839', '#000']);
-    map.setPaintProperty('changing-new-york', 'circle-radius', ['case', ['==', ['id'], this.state.id], 10, 5]);
-  }
-
-  inactiveFeature = (map) => {
-    map.setPaintProperty('changing-new-york', 'circle-color', ['case', ['!=', ['id'], this.state.id], '#000', '#000']);
-  }
-
   componentDidMount() {
+
     const mapContainer = document.getElementById('map');
+
     const MAPBOXGL = window.mapboxgl;
+
+    const popup = new MAPBOXGL.Popup({ offset: [0, -15] })
 
     MAPBOXGL.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
@@ -58,58 +34,37 @@ class Map extends Component {
       interactive: false
     });
 
-    map.on('load', () => {
-      map.addSource('changing-new-york', {
-        'type': 'geojson',
-        'data': geojson.geojson
+    geojson.geojson.features.forEach((marker) => {
+
+      const el = document.createElement('div');
+
+      el.className = 'marker';
+
+      if(marker.id === this.state.id) {
+        el.setAttribute("id", "active");
+      }
+
+      el.addEventListener('mouseover', () => {
+        popup.setLngLat(marker.geometry.coordinates)
+          .setHTML('<p>' + marker.properties.title + '</p>')
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map)
       });
 
-      map.addLayer({
-        'id': 'changing-new-york',
-        'layout': {},
-        'paint': {},
-        'source': 'changing-new-york',
-        'type': 'circle'
-      })
-
-      this.activeFeature(map, this.state.id)
-    })
-
-    const popup = new MAPBOXGL.Popup({ offset: [0, -15] })
-
-    map.on('mousemove', (e) => {
-
-      map.getCanvas().style.cursor = 'pointer';
-
-      this.getFeatures(e, map)
-
-      if (!this.state.features.length) {
+      el.addEventListener('mouseleave', () => {
         popup.remove();
-        return;
-      };
-      this.activeFeature(map, this.state.feature.id)
-      popup.setLngLat(this.state.feature.geometry.coordinates)
-        .setHTML('<p>' + this.state.feature.properties.title + '</p>')
-        .setLngLat(this.state.feature.geometry.coordinates)
-        .addTo(map)
+      });
+
+      el.addEventListener('click', () => {
+        document.getElementById("active").removeAttribute("id");
+        el.setAttribute("id", "active");
+        this.handleClick(marker.properties.UUID)
+      });
+
+      new MAPBOXGL.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
     });
-
-    map.on('mouseleave', (e) => {
-      this.getFeatures(e, map)
-
-      if (this.state.feature !== undefined) {
-        this.inactiveFeature(map);
-      }
-    })
-
-    map.on('click', (e) => {
-      this.getFeatures(e, map)
-
-      if (this.state.feature !== undefined) {
-        this.activeFeature(map, this.state.feature.id)
-        this.handleClick(this.state.feature.properties.UUID);
-      };
-    })
   }
 
   render() {
