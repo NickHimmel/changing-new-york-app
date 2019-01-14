28274506abbc67db51f1515d98d2c0db89e55f94
NickHimmel <himmel.nick@gmail.com>
2019-01-14 13:09:14 -0500

    Add instructions and conditional statement to find note function in case of undefined

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -12,6 +12,10 @@ const Intro = () => {
         <p className={styles.text}>New York City was born on the water. A verdant paradise with accessible waterfronts led to the first settlements in what is now Lower Manhattan. In the 19th century goods were moved by boat and New York’s river access made the city rich. Industrial titans built a grand and gilded city. But the steep price for that wealth was poison, as industry polluted the rivers and the air.</p>
         <p className={styles.text}>Throughout the 20th century New York moved away from heavy industry. The financial sector, Wall Street, excelled. Skyscrapers rose ever higher, office towers holding the new engines of the city. The waterfront was mostly abandoned. Robert Moses built his great highway system along the rivers and large scale housing complexes were erected for the poor.</p>
         <p className={styles.text}>Now Wall Street is the financial capital of the world. New York is a luxury city. The rivers have been cleaned, the city and private developers are reclaiming the waterfront. The once hard-working piers are being replaced with active community spaces and Instagram-worthy destinations. High-end residential towers now sprout along the rivers. Ferries have returned to the waters. The rejuvenation has been a wild success, but the future looms ominously. Global warming is happening and the waters are rising. Soon the city’s coastline will have to be fundamentally reimaged. As Berenice Abbott captured a rapidly disappearing piece of New York, I might have as well.</p>
+        <div>
+          <span className={styles.pointer}>&#9755;</span>
+          <span className={styles.instructions}>To view images and information about a location, click the dot on the map</span>
+        </div>
       </div>
     </div>
   );
