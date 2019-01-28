bfc2db84ff0c1b0a30bb9c60b3bc9b2ee7e9634f
NickHimmel <himmel.nick@gmail.com>
2019-01-28 14:35:17 -0500

    Update App from module to scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'normalize.css';
 import './base-styles/global.css'
-import styles from './App.module.css';
+import styles from './App.scss';
 import Intro from './components/Intro/Intro.js'
 import Map from './components/Map/Map.js';
 import Comparisons from './components/Comparisons/Comparisons.js';
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const App = () => {
   return (
-    <div className={styles.container}>
+    <div className="app">
       <Intro />
       <Map />
       <Comparisons />
