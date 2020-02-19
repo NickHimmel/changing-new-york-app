1e1628771e4d32157810a153af3b2edbc5e6073b
NickHimmel <himmel.nick@gmail.com>
2020-02-19 16:52:59 -0500

    Updates

---
+++ precache-manifest.e1793e724d8bec03e715b16947b8f177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dce55552a3285811c1b0",
+    "url": "/changing-new-york-app/static/css/main.dc10fd9b.chunk.css"
+  },
+  {
+    "revision": "dce55552a3285811c1b0",
+    "url": "/changing-new-york-app/static/js/main.dce55552.chunk.js"
+  },
+  {
+    "revision": "4d874606c2ff9a47e8ac",
+    "url": "/changing-new-york-app/static/css/1.bcb36134.chunk.css"
+  },
+  {
+    "revision": "4d874606c2ff9a47e8ac",
+    "url": "/changing-new-york-app/static/js/1.4d874606.chunk.js"
+  },
+  {
+    "revision": "675ee4cb62fce1d80a96",
+    "url": "/changing-new-york-app/static/js/runtime~main.675ee4cb.js"
+  },
+  {
+    "revision": "5bc7fdac92764d3aa791e16fb20e95a4",
+    "url": "/changing-new-york-app/index.html"
+  }
+];
